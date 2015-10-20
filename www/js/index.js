748e57753e66f97a4ebd615a7ee71bca09f212b2
georgebrown <cyrus_b_uk@server>
2015-10-20 15:42:45 +0200

    Added list items

--- www/js/index.js
+++ www/js/index.js
@@ -33,8 +33,11 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByName("gray");
     },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
